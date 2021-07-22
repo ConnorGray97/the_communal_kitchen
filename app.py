28fa8c3551562b38bd1f353274014092801c38fd
ConnorGray97 <connor-gray@hotmail.com>
2021-07-22 14:29:16 +0000

    Fix minor bug in app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
+@app.route("/get_recipes")
+def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
