abb6a749141e40867024a70af3863b72b3d56d57
ConnorGray97 <connor-gray@hotmail.com>
2021-08-11 21:13:50 +0000

    Styled add recipe form and add delete input-field functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,45 +1,51 @@
 // ------------------------ Materialize Initialization
 
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
+  });
+});
+
+$(document).ready(function () {
+  $('select').formSelect();
+});
+
+// ------------------------ End Materialize Initialization
+
 $(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-  });
+  $('flashes').delay(5000).slideUp(300);
+});
 
-  $(document).ready(function(){
-    $('select').formSelect();
-  });
+// Credit: 
+// Code inspired from https://github.com/rebeccatraceyt/bake-it-til-you-make-it/blob/master/static/js/recipe.js
 
-  // ------------------------ End Materialize Initialization
 
-  // Credit: 
-  // Code inspired from https://github.com/rebeccatraceyt/bake-it-til-you-make-it/blob/master/static/js/recipe.js
+let maxIngredients = 30;
+let ingredientRow = document.getElementById('ingredient-row');
+let addIngredient = document.getElementById('add-ingredient');
+let ingredient = 1;
 
-  
-  let maxIngredients = 30;
-  let ingredientRow = document.getElementById('ingredient-row');
-  let addIngredient = document.getElementById('add-ingredient');
-  let ingredient = 1;
+let maxSteps = 30;
+let methodRow = document.getElementById('prep-step');
+let addStep = document.getElementById('add-step');
+let step = 1;
 
-  let maxSteps = 30;
-  let methodRow = document.getElementById('prep-step');
-  let addStep = document.getElementById('add-step');
-  let step = 1;
+// Append new ingredient div
 
-  // Append new ingredient div
+const appendIngredient = (e) => {
+  e.preventDefault();
 
-  const appendIngredient = (e) => {
-    e.preventDefault();
-
-    if (ingredient < maxIngredients) {
-      ingredient++;
-      let newIngredientField = document.createElement('div');
-      newIngredientField.innerHTML = `
-      <div id="ingredient-row">
+  if (ingredient < maxIngredients) {
+    ingredient++;
+    let newIngredientField = document.createElement('div');
+    newIngredientField.innerHTML = `
+    
       <div class="input-field col s12 m8 l8">
           <input id="ingredient_name_1" name="ingredient_name" type="text" maxlength="100">
           <label for="ingredient_name_1">Ingredient</label>
       </div>
       <!-- Amount -->
-      <div class="input-field col s4 m1 l2 right">
+      <div class="input-field col s4 m1 l2">
           <input id="amount_1" type="text" maxlength="10">
           <label for="amount_1">Amount</label>
       </div>
@@ -48,13 +54,22 @@ $(document).ready(function(){
           <input id="unit_1" type="text" maxlength="10">
           <label for="unit_1">Unit</label>
       </div>
-  </div>`;
-      ingredientRow.append(newIngredientField);
+      
+      <a href="#" class="remove-field text-shadow"><i class="fas fa-trash-alt fa-2x"></i></a>
+      
+  `;
+    ingredientRow.append(newIngredientField);
 
 
-    }
-  };
+  }
+};
 
-  // Event Listeners
+$(ingredientRow).on('click', '.remove-field', function(e) {
+  e.preventDefault();
+  $(this).parent('div').remove();
+  ingredient--;
+});
 
-  addIngredient.addEventListener('click', appendIngredient);
+// Event Listeners
+
+addIngredient.addEventListener('click', appendIngredient);
