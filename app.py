b9684d7a881b89908a39de4259c8340a095b4044
ConnorGray97 <connor-gray@hotmail.com>
2021-08-13 02:04:48 +0000

    Remove pagination declaration

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_paginate import Pagination, get_page_args
 if os.path.exists("env.py"):
     import env
 
